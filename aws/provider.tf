4364e94bfbde17153b8d2e2c17ab9518f5c284d0
Corcoran Smith <corcoran.smith@slalom.com>
2020-08-13 20:03:08 +0100

    Removing credentials_profile

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 # shared_credentials_file = var.aws_credentials_path
-  profile = var.aws_credentials_profile
+# profile = var.aws_credentials_profile
   region     = var.aws_region
 }
 
