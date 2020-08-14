047c85c35ed63009b5b9c3bf70dadb8097b354ef
Corcoran Smith <corcoran.smith@slalom.com>
2020-08-14 02:57:11 +0100

    Public to private

--- aws/output.tf
+++ aws/output.tf
@@ -3,9 +3,9 @@ output "rancher_server_url" {
 }
 
 output "rancher_node_ip" {
-  value = aws_instance.rancher_server.public_ip
+  value = aws_instance.rancher_server.private_ip
 }
 
 output "workload_node_ip" {
-  value = aws_instance.quickstart_node.public_ip
+  value = aws_instance.quickstart_node.private_ip
 }
